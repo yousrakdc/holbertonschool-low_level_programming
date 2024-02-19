5f2d3957ce014e521aa078715f5e034ea8f3b60b
yousrakdc <yousrakdc@gmail.com>
2024-02-19 12:30:26 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,7 +9,7 @@ float floatType;
 printf("Size of a char: %zu byte\n", sizeof(charType));
 printf("Size of an int: %zu bytes\n", sizeof(intType));
 printf("Size of a long int: %zu bytes\n", sizeof(longintType));
-printf("Size of a long longi nt: %zu bytes\n", sizeof(longlongintType));
+printf("Size of a long long int: %zu bytes\n", sizeof(longlongintType));
 printf("Size of a float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
