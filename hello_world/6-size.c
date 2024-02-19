0ac2733c5444007a9a9f83475e7aa622b98a6b49
yousrakdc <yousrakdc@gmail.com>
2024-02-19 12:34:26 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {char charType;
 int intType;
