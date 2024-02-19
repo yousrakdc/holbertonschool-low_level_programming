9f04e5e303bdd6c08307e4f4602a8ab97218f099
yousrakdc <yousrakdc@gmail.com>
2024-02-19 14:53:46 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-int main(void){
-
-	char = letter 'a';
-	while (letter <= 'z') {
-	
-	putchar(letter);
-	letter ++;
-
-	}
-	
-putchar('\n');
-return (0);
+int main() {
+    char letter = 'a';
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+    putchar('\n');
+    return 0;
 }
+
