efca61672219ce1faad4d4b35467f5a08593d19a
yousrakdc <yousrakdc@gmail.com>
2024-02-20 10:20:40 +0000

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in reversed in lowercase followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'a')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
