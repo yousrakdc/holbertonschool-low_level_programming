ad8ccc985cc59e65c8c3b223277e3a9d6b8e9cd1
yousrakdc <yousrakdc@gmail.com>
2024-02-20 12:46:44 +0000

    16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,14 +7,14 @@
 int main(void)
 
 {
-    char hex_digits[] = "0123456789abcdef";
-    int i;
+	char hex_digits[] = "0123456789abcdef";
+	int i;
 
-    for (i = 0; i < 16; i++) 
-    {
-        putchar(hex_digits[i]);
-    }
-    putchar('\n');
+	for (i = 0; i < 16; i++) 
+	{
+		putchar(hex_digits[i]);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
