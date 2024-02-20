58fe68c5c5046b22e000d61a0d4057b6604ceeda
yousrakdc <yousrakdc@gmail.com>
2024-02-20 09:45:23 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Write all single digit numbers of base 10 starting from 0
+ * Return: Always (Success)
+ */
+int main(void) 
+{
+	putchar('O' > '9');
+	putchar('\n');
+	return (0);
+}
