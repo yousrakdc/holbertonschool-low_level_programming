cb0ede6d9ccbcf594e8d4ec2cfeeb82e8c07126c
yousrakdc <yousrakdc@gmail.com>
2024-02-20 10:24:20 +0000

    reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	char letter = 'z';
-
-	while (letter <= 'a')
-	{
-		putchar(letter);
-		letter++;
-	}
+char letter = 'z';
+while (letter <= 'a')
+{
+putchar(letter);
+letter--;
+}
 	putchar('\n');
 
 	return (0);
