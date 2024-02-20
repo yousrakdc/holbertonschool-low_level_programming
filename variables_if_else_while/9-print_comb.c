0658d559ed714e09f861c2fead6f004720aba6f7
yousrakdc <yousrakdc@gmail.com>
2024-02-20 14:10:58 +0000

    number

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	for (number = 0; number < 10; number++)
 	{
 		putchar((number % 10) + '0');
-		putchar(',');
 
 		if (number == 9)
-			continue;
+			continuei;
+		putchar(', ');
 	}
 	putchar('\n');
 
