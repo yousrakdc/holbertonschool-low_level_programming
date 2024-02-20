fe1e456c4a105953a1340d3caca2f63de8ef4ff0
yousrakdc <yousrakdc@gmail.com>
2024-02-20 14:03:31 +0000

    number

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always (Success)
+ */
+int main(void)
+
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number + 10%) '0');
+
+		if (number == 9);
+			continue
+	}
+	putchar(',');
+	putchar('\n');
+
+	return (0);
+
+}
