e6d8eeccb812ab6ead0b400dbcd10d1e030f8cc3
yousrakdc <yousrakdc@gmail.com>
2024-02-20 09:04:11 +0000

    digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+if (x < 9)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
+}
