b2d57ded7c0497484cbc45c8f2fb15307d8b47b6
yousrakdc <yousrakdc@gmail.com>
2024-02-20 12:42:23 +0000

    16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Write all single digit numbers of base 10 starting from 0
+ * Return: Always (Success)
+ */
+int main(void)
+
+{
+    char hex_digits[] = "0123456789abcdef";
+    int i;
+
+    for (i = 0; i < 16; i++) 
+    {
+        putchar(hex_digits[i]);
+    }
+    putchar('\n');
+
+    return (0);
+}
