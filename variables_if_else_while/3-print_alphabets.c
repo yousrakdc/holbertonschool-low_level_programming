825a60516150e2e828b6a358905f63ed10038480
yousrakdc <yousrakdc@gmail.com>
2024-02-20 08:28:04 +0000

    lower then upper case

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,28 +3,24 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase then uppercase followed by a new line
+ * Description: Prints the alphabet followed by a new line
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-
-    letter = 'A'; /* Resetting letter to start from 'A' for uppercase letters*/
-
-    while (letter <= 'Z')
-    {
-        putchar(letter);
-        letter++;
-    }
-    putchar('\n');
-
-    return 0;
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+letter = 'A'; /* Resetting letter to start from 'A' for uppercase letters*/
+while (letter <= 'Z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
 }
