d19164c403c63126a47b823d9147a9f6a2ae3a1e
yousrakdc <yousrakdc@gmail.com>
2024-02-20 09:52:15 +0000

    numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -4,11 +4,13 @@
  * main - Write all single digit numbers of base 10 starting from 0
  * Return: Always (Success)
  */
-int main(void) 
+int main(void)
 {
-	putchar('O' > '9');
-{
+	int i;
+for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
 	putchar('\n');
-}
-	return (0);
+return (0);
 }
