dd4bc47c369a81f90de89c3d67545d8ededfe1a8
yousrakdc <yousrakdc@gmail.com>
2024-02-20 10:54:20 +0000

    reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,13 @@
  * Description: Prints the alphabet in reversed in lowercase followed
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char letter;
-	for (letter = 'z'; letter >= 'a'; letter--) 
-	{	
-		putchar(letter);
-	}
+	for (letter = 'z'; letter >= 'a'; letter--)
+{
+	putchar(letter);
+}
 	putchar('\n');
 	return (0);
 }
