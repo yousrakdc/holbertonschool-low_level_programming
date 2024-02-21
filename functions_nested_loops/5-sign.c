a6de8dac50671aedaffc4df4e67eb7997b18fc8c
yousrakdc <yousrakdc@gmail.com>
2024-02-21 13:58:47 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign: prints the sign of a number.
+ * print_sign -  prints the sign of a number.
  * @n: number to be verified
- * return: print_sign 1 for positive n, -1 fo negative, 0 for anything else
+ * Return: print_sign 1 for positive n, -1 for negative, 0 for anything else
  */
 
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
