232b5930990aa4e5501c2d5b381686ada1864c1a
yousrakdc <yousrakdc@gmail.com>
2024-02-21 08:32:55 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - print alphabet in lowercase
+ * Return : Always 0
+ */
+
+int print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
