b9e07f59859b01ddf02fa1694412cf5b64d8f410
yousrakdc <yousrakdc@gmail.com>
2024-02-21 09:35:58 +0000

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -18,5 +18,4 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
-	return(void)
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,22 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/* _putchar - writes the character c to stdout */
 int _putchar(char c);
-
-#endif /* MAIN_H */
-
 void print_alphabet(void);
-
-{
-        char letter = 'a';
-
-        while (letter <= 'z')
-        {
-                _putchar(letter);
-                letter++;
-        }
-
-        _putchar('\n');
-        return(void)
-}
