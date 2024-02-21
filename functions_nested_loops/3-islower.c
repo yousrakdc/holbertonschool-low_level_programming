47f2d600b2a939f846bc637c7119fafb0e1ff8cf
yousrakdc <yousrakdc@gmail.com>
2024-02-21 10:05:46 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower - checks for lower case character
+ * return : 1 for lowercase character
+ */
+
+int _islower(int c)
+
+{
+	if (c >+ 97 && c <= 122)
+	{
+	
+	return (1);
+	}
+	return (0);
+}
