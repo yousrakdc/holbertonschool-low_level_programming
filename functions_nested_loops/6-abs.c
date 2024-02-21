b533414501ed8eb72f9fbafbc029437467538210
yousrakdc <yousrakdc@gmail.com>
2024-02-21 14:10:29 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ * Return: Always 0
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+		return -n;
+	}
+	else
+	{
+		return n;
+	}
+}
