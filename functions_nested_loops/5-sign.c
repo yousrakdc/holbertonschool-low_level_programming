db857f7a65ea2fc195eea1b251953a5bb543ca9f
yousrakdc <yousrakdc@gmail.com>
2024-02-21 13:42:32 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(98);
-		return('1');
+		_putchar(43);
+		return(1);
 	}
 	else if (n < 0)
 	{
-	_putchar(99);
-		return('-');
+	_putchar(45);
+		return(-1);
 	}
 	else  (n = 1);
 	{
 		_putchar(48);
-		return('1');
+		return(0);
 	}
 }
