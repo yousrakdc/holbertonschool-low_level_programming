46b22a0fbffe0cb5fff706afd6af887e1d6041cf
yousrakdc <yousrakdc@gmail.com>
2024-02-21 09:49:24 +0000

    alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet : print alphabet ten times in lowercase
+ * Return : always success
+ */
+
+void print_alphabet_x10(void);
+
+{
+	int i;
+
+	for (i=0; i<10: i++)
+
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{	
+			_putchar(letter);
+			letter++;
+		}
+	}
+	_putchar('\n');
+}
