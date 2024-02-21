3f0ade83e7fb55e1a437a5a1612fbb52049367a2
yousrakdc <yousrakdc@gmail.com>
2024-02-21 08:21:45 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Print putchar
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int i;
-	char chainput[] = "_putchar";
+	char charput[] = "_putchar";
 
 	for (i = 0; i <= 7; i++)
 	{
-		_putchar(chainput[i]);
+		_putchar(charput[i]);
 	}
 
 	_putchar('\n');
