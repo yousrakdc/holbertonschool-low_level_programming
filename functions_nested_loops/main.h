dd9b14b3b410f6948c6861a1d1bc1cee66931a77
yousrakdc <yousrakdc@gmail.com>
2024-02-21 08:34:15 +0000

    main

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,3 +6,15 @@ int _putchar(char c);
 
 #endif /* MAIN_H */
 
+int print_alphabet(void);
+{
+        char letter = 'a';
+
+        while (letter <= 'z')
+        {
+                putchar(letter);
+                letter++;
+        }
+        putchar('\n');
+        return (0);
+}
