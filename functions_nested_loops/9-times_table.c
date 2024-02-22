fe724a7cdd4d47b44abaee63e60867f5d8d51074
yousrakdc <yousrakdc@gmail.com>
2024-02-22 09:54:44 +0000

    times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,17 +3,23 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- * @n:  number 
  * Return: always success
  */
 
 void times_table(void)
+{       
+	int i, j;
 
-}
-	int n;
-
-	for (int i = 1; i <= 9; ++i)
+	for (i = 0; i <= 9; i++)
 	{
-	printf("%d * %d = %d \n", n, i, n * i);
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%2d", i * j);
+			if (j < 9)
+			{
+				printf(", ");
+			}
+		}
+		printf("$\n");
 	}
 }
