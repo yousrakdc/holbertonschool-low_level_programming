6e9ccebc932517d2e4f7014e0eea50c976f8b36e
yousrakdc <yousrakdc@gmail.com>
2024-02-22 10:18:31 +0000

    times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,25 +12,32 @@
 void times_table(void)
 
 {
-	char i = row; j = column; z = result;
+	int i, j, z;
 
-	int z = i * j;
-	if (z >= 9) 
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(z / 10 + '0');
-		putchar(z % 10 + '0');
-	} else
-	{
-		putchar(z + '0');
+		for (j = 0; j <= 9; j++)
+		{
+			z = i * j;
+
+			if (z >= 9) 
+			{
+				putchar(z / 10 + '0');
+				putchar(z % 10 + '0');
+			} else
+			{
+				putchar(z + '0');
+			}
+
+			if (j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+				if (z < 10) putchar(' ');
+			}
+
+			putchar('$');
+			putchar('\n');
+		}
 	}
-
-	if (j < 9)
-	{
-		putchar(',');
-		putchar(' ');
-		if (z < 10) putchar(' ');
-	}
-
-	putchar('$');
-	putchar('\n');
 }
