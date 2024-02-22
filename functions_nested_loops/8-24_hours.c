b970daad9c7b04aea09f389c0f590b67496cff53
yousrakdc <yousrakdc@gmail.com>
2024-02-22 09:26:09 +0000

    jack

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++);
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++);
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
