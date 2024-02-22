a56a781728d14f856ecc15a1aa234b85b7a66dfa
yousrakdc <yousrakdc@gmail.com>
2024-02-22 10:02:23 +0000

    times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,28 +3,30 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
+ * @i: row number
+ * @j: column number
  * Return: always success
  */
 
 void times_table(void)
 
 {	int result = i * j;
-            if (result >= 10) 
-	    {
-                putchar(result / 10 + '0');
-                putchar(result % 10 + '0');
-            } else {
-                putchar(result + '0');
-            }
+	if (result >= 9) 
+	{
+		putchar(result / 10 + '0');
+		putchar(result % 10 + '0');
+	} else
+	{
+		putchar(result + '0');
+	}
 
-            if (j < 9)
-            {
-                putchar(',');
-                putchar(' ');
-                if (result < 10) putchar(' ');
-            }
-        }	
-        putchar('$');
-        putchar('\n');
-    }
+	if (j < 9)
+	{
+		putchar(',');
+		putchar(' ');
+		if (result < 10) putchar(' ');
+	}
+
+	putchar('$');
+	putchar('\n');
 }
