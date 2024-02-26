b4885f2b333204032eb082088c6d73427e10a48c
yousrakdc <yousrakdc@gmail.com>
2024-02-26 12:15:33 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: a number
+ * Result: n is 0 or less, the function should only print \n
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 1 ; i <= n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
