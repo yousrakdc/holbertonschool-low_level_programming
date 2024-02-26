39ae5c1c9054e38b4b0b6ca3af82ed65b68a66b4
yousrakdc <yousrakdc@gmail.com>
2024-02-26 09:51:53 +0000

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: a number
+ * @b: another number
+ * Return: always success
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
