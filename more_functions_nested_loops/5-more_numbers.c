753b109039b0c8c6443a5f947bceceb71591125f
yousrakdc <yousrakdc@gmail.com>
2024-02-26 11:56:19 +0000

    too many times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,24 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * @i: number
  * Return: always success
  */
 
 void more_numbers(void)
 
 {
+	int i, n;
 
-	int i = 0;
-
-	while (i = 0; i <= 14; i++) 
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
-	
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
