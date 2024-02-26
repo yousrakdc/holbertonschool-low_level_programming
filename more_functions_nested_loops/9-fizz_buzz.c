0f02e46c58e7a81fecd2ca570d70d010cf632f12
yousrakdc <yousrakdc@gmail.com>
2024-02-26 13:23:19 +0000

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
