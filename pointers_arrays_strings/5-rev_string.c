15d2f2e6d0665d81ff0094a9e7c3a683a9ff829c
yousrakdc <yousrakdc@gmail.com>
2024-02-27 10:23:21 +0000

    reverse string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,6 +3,7 @@
 
 /**
  * rev_string - function that reverses a string
+ * @s: string
  * Return: allways success
  */
 
@@ -10,18 +11,22 @@ void rev_string(char *s)
 
 {
 	int length = 0;
+	int start = 0;
+	int end;
+
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	int start = 0;
-	int end = length - 1;
+	end = length - 1;
 
 	while (start < end)
 	{
 		char temp = s[start];
+
 		s[start] = s[end];
+
 		s[end] = temp;
 		start++;
 		end--;
