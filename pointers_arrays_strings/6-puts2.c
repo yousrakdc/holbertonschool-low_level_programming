fc9e8af0a75fccb00c0560d712037a3bb06cd7fa
yousrakdc <yousrakdc@gmail.com>
2024-02-27 11:56:57 +0000

    puts

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ * Return: always success
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
