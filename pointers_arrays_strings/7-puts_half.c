8d8904b5f0899d84b0c9f38f8433855a5e63abfd
yousrakdc <yousrakdc@gmail.com>
2024-02-27 12:35:48 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: always success
+ */
+
+void puts_half(char *str)
+
+{
+	int length, i;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (length % 2)
+	{
+		for (n = (length - 1) / 2; n < length - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+
+_putchar('\n');
+}
