94d3dc221b5102d1f4409b8feecb149560133452
yousrakdc <yousrakdc@gmail.com>
2024-02-28 09:25:53 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,35 @@
 int _atoi(char *s)
 
 {
-	s = " -9885";
-	i = _atoi(s);
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-	printf("i = %d\n",i);
+	if (s[0] == '-') 
+	{
+		sign = -1;
+		i++;
+	}
+
+	if (s[0] == '+') 
+	{
+		i++;
+	}
+
+	while (s[i] != '\0') 
+	{
+		if (s[i] >= '0' && s[i] <= '9') 
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else 
+		{
+			break;
+		}
+		i++;
+	}
+
+	result = result * sign;
+
+	return result;
 }
