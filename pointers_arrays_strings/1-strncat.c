f471c0ba896b1eb0e95d88f576d8b8fc5502f9cd
yousrakdc <yousrakdc@gmail.com>
2024-02-28 12:59:52 +0000

    strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,23 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
-	int  dest_len = strlen(dest);
-	int  i;
+	int i, l;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	i = 0;
+	l = 0;
+	
+		while (dest[i] != '\0')
+		i++;
+
+	while (src[l] != '\0' && l < n)
+	{
+		dest[i] = src[l];
+		i++;
+		l++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
+
 }
