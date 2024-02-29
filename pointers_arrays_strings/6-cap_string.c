bafdcfd9d3b42e828351af5cc7c736aed70567e7
yousrakdc <yousrakdc@gmail.com>
2024-02-29 13:00:47 +0000

    capitalize

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: string to capitalize
+ * Return: capitalized string
+ */
+
+char *cap_string(char *c)
+{
+	int i = 0;
+	int j;
+	char separators[] = " \n\t,;.!?\"(){}";
+
+
+	if (c[0] >= 'a' && c[0] <= 'z')
+		c[0] -= 32;
+
+	for (; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (c[i] == separators[j])
+			{
+				if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+					c[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (c);
+}
