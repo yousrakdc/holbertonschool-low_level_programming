24810eeae6fa44720d0ceac11d5c53f058694496
yousrakdc <yousrakdc@gmail.com>
2024-02-29 10:07:41 +0000

    string toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Convert a string to uppercase
+ * @p: Pointer to string
+ * Return: always success
+ */
+
+
+char *string_toupper(char *p)
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 'p' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+	}
+
+	return (p);
+}
