78307761a29d03ce64f7b652eaa51ff3cf7ea2f3
yousrakdc <yousrakdc@gmail.com>
2024-03-04 14:47:15 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; needle[i]; i++)
 		{
-			if (haystack[i] != needle[s])
+			if (haystack[i] != needle[i])
 			break;
 		}
 		if (i != s)
