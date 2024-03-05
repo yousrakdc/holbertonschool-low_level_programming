54eb83425db2c422b2d350f395901672b77dd386
yousrakdc <yousrakdc@gmail.com>
2024-03-05 13:15:47 +0000

    hi

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -12,10 +12,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	 {
-		putchar('\n');
+		_putchar('\n');
 		return; 
 	 }
-	putchar(*s);
+	_putchar(*s);
 	s++;
 	_puts_recursion(s);
 }
--- recursion/main.h
+++ recursion/main.h
@@ -1,4 +1,4 @@
 void _puts_recursion(char *s);
-int _putchar(char);
+int _putchar(char c;
 int _strlen(char *s);
 void _puts(char *str);
