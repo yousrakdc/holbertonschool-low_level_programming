ea39496c1922d3bf72f135f48d0a31d2558b5ccf
yousrakdc <yousrakdc@gmail.com>
2024-03-05 09:25:44 +0000

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer
+ * Return: chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+}
