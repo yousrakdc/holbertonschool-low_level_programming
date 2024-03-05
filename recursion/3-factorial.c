6612ab2698c399ebbff7c192d032e779b097d1f3
yousrakdc <yousrakdc@gmail.com>
2024-03-05 13:56:19 +0000

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,16 +10,16 @@
 int factorial(int n)
 
 {
-	if (n < 0) 
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0) 
+	else if (n == 0)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
-		return n * factorial(n - 1);
+	return n * factorial(n - 1);
 	}
 }
