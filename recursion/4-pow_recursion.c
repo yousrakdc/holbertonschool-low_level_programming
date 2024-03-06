ce2db0ca18470d7aa293538cd570789390b6d8b6
yousrakdc <yousrakdc@gmail.com>
2024-03-06 09:36:18 +0000

    recursion power

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,7 +13,8 @@ int _pow_recursion(int x, int y)
 {
 	if (x == 0)
 		return (0);
-	if (y < 0)
-		return (-1);
-	return (0);
+	else if (y < 0)
+		return x * _pow_recursion(x, y, -1);
+	else
+		return 1 / (x * _pow_recursion(x, -y -1));
 }
