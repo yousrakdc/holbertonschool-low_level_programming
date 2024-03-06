51f093a6efa152a49f37cd3a793bb91625a413bf
yousrakdc <yousrakdc@gmail.com>
2024-03-06 12:46:11 +0000

    prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,28 +3,32 @@
 
 /**
  * is_prime_number - prime number
+ * is_prime_number_helper - helper
  * @n: number
  * Return: 1 if the input integer is a prime number, otherwise return 0
  */
 
-int is_prime_number(int n)
+int is_prime_number_helper(int n, int i)
 
 {
-	int i = 0;
-
-	if (i == 1)
+	if (i <= 1)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
 		if (n % i == 0)
 		{
-			return 0;
+			return (0);
 		}
 		else
 		{
-			return (is_prime_number(n, i - 1));
+			return (is_prime_number_helper(n, i - 1));
 		}
 	}
 }
+
+int is_prime_number(int n)
+
+{
+	return (s_prime_number_helper(n, n - 1));
