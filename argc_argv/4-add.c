6650693c0e5e6f92829f2953fe5f39d3b00b1695
yousrakdc <yousrakdc@gmail.com>
2024-03-07 09:11:36 +0000

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: print a number followed by a new line
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
