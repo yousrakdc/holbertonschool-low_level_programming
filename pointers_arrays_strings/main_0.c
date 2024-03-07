a56ffeaabcf6709c76c2abb5e67533fd6a4e825e
yousrakdc <yousrakdc@gmail.com>
2024-03-07 13:55:03 +0000

    set string

--- pointers_arrays_strings/main_0.c
+++ pointers_arrays_strings/main_0.c
@@ -1,1 +1,20 @@
 int _putchar(char c);
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
+
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
+}
