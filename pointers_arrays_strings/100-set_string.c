a66e2c456809a4d6fb1b2c08db251000c5a62c3c
yousrakdc <yousrakdc@gmail.com>
2024-03-08 10:43:15 +0000

    set string

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -10,21 +10,3 @@ void set_string(char **s, char *to)
 {
 	*s = to;
 }
-
-int main()
-
-{
-	int i;
-
-	char *s = NULL;
-	char str[] = "Bob Dylan";
-
-	set_string(&s, str);
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
-	return (0);
-}
