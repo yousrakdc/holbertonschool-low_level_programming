76d0d127336473222796c0d5a04123acf0e2f007
yousrakdc <yousrakdc@gmail.com>
2024-03-11 14:42:20 +0000

    nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,10 @@
 
 /**
  * string_nconcat - function that concatenates two strings.
- * Return: returned pointer where there's s1, followed by the first n bytes of s2 and NULL
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number
+ * Return: pointer where there's s1, then the first n bytes of s2 and NULL
  * if n >= lenght of s2, use entire string s2
  * if NULL is passed, treat as empty string
  */
@@ -14,17 +17,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
+	size_t len_s1, len_s2, concat_len;
+	char *result;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
 
-	size_t concat_len = len_s1 + (n >= len_s2 ? len_s2 : n);
+	concat_len = len_s1 + (n >= len_s2 ? len_s2 : n);
 
-	char *result = malloc(concat_len + 1);
+	result = malloc(concat_len + 1);
 	if (result == NULL)
 	{
 		return (NULL);
@@ -32,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	strcpy(result, s1);
 
-	strncat(result, s2, n >= len_s2 ? sen_s2 : n);
+	strncat(result, s2, n >= len_s2 ? len_s2 : n);
 
 	result[concat_len] = '\0';
 
