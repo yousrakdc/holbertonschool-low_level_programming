86eb392924b024043eddae8d8b487fafa2005b66
yousrakdc <yousrakdc@gmail.com>
2024-03-13 10:40:52 +0000

    print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,6 +19,6 @@ void print_dog(struct dog *d)
 
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
