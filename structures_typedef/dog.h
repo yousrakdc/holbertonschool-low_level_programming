9f1fb71cb057cb90c962e25d79ed0cb901e2b30c
yousrakdc <yousrakdc@gmail.com>
2024-03-15 09:18:25 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,16 @@
  * Description: structure to store dog info
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
