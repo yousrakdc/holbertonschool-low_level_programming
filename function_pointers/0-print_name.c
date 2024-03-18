26bb2b0e5f0770f259b91ad91392452239b19603
yousrakdc <yousrakdc@gmail.com>
2024-03-18 09:57:51 +0000

    hodor

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <ctype.h>
+
+void print_name_uppercase(char *c)
+{
+	putchar(toupper(*c));
+}
 
 /**
  * print_name - prints a name
@@ -16,5 +22,12 @@ void print_name(char *name, void (*f)(char *))
 		f(name);
 		name++;
 	}
+
 	putchar('\n');
+}
+
+int main()
+{
+	print_name("Hodor", &print_name_uppercase);
+	return(0);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);
+void print_name_uppercase(char *c);
 void print_name(char *name, void (*f)(char *));
 #endif
