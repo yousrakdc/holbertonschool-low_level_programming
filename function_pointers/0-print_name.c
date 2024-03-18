b4613913f1e2177cd8136f9999807c49b2907c8f
yousrakdc <yousrakdc@gmail.com>
2024-03-18 09:25:25 +0000

    docs

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,2 +1,33 @@
-:x
+#include "function_pointers.h"
+#include <stdio.h>
 
+/**
+ * print_name - prints a name
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	while (name != '\0')
+	{
+		putchar(*name);
+		name++;
+	}
+	putchar('\n');
+
+typedef void (*NamePrinter)(char *);
+
+/*
+ * main - main function
+ * @printer: prints the name
+ */
+
+int main()
+{
+	NamePrinter printer;
+
+	printer = &printName;
+	(*printer)("Bob");
+	return(0);
+}
