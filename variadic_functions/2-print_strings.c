3b1e27a31a547b9b118c09a95591002078304e00
yousrakdc <yousrakdc@gmail.com>
2024-03-20 08:46:12 +0000

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", s);
 
-		if (separator != NULL && i != n - 1)
+		if (i < n - 1 && separator)
 		{
 			printf("%s", separator);
 		}
