b3f32c90d436a2546533b9e1fa21da0015927727
yousrakdc <yousrakdc@gmail.com>
2024-03-20 08:50:18 +0000

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * print_strings - prints strings, followed by a new line
@@ -11,15 +12,15 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	const char *s;
 	unsigned int i;
+	char *s;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, const char *);
+		s = va_arg(args, char *);
 
 		if (!s)
 			printf("nil");
