1e54a1ae4682c3236cfd422f9eb9f8198a03203a
yousrakdc <yousrakdc@gmail.com>
2024-03-22 08:51:27 +0000

    please

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -43,7 +43,8 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		return (0);
+		printf("Error\n");
+		exit(100);
 	}
 	return (a / b);
 }
@@ -58,7 +59,8 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		return (0);
+		printf("Error\n");
+                exit(100);
 	}
 	return (a % b);
 }
