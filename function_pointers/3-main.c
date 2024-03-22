1dee1e05a18fdd3b94349eec76ee46cd5ebadbc6
yousrakdc <yousrakdc@gmail.com>
2024-03-22 09:07:17 +0000

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 
 /**
-  * main - main function
-  * @argc: argc
-  * @argv: argv
-  * Return: Nothing
-  */
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: Nothing
+ */
 
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int (*oprt)(int, int), a, b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	oprt = get_op_func(argv[2]);
-
 	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+		printf("Error\n"), exit(99);
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%i\n", oprt(a, b));
 	return (0);
-}
+}	
