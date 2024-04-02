fa05314483664da982109770bdf660a74f96a6b1
yousrakdc <yousrakdc@gmail.com>
2024-04-02 11:44:40 +0000

    node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <stddef.h>
 
@@ -27,13 +28,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-	{
-		free(new_str);
 		return (NULL);
-	}
 
 	new_node->str = new_str;
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -52,10 +50,10 @@ list_t *add_node_end(list_t **head, const char *str)
 }
 
 /**
-  * _strlen - gets length of the string
-  * @s: string
-  * Return: length of the string
-  */
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
+ */
 int _strlen(const char *s)
 {
 	int i;
