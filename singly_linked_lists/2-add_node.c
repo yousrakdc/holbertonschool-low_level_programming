ea840bc81e1cb9b58e020e009e81ad8931160bb8
yousrakdc <yousrakdc@gmail.com>
2024-04-02 09:59:48 +0000

    added

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -39,3 +39,37 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new_node);
 }
+
+/**
+  * _strlen - gets length of the string
+  * @s: string
+  * Return: length of the string
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+  * _strdup - recreation of string duplicate function
+  * @src: source of string to duplicate
+  * Return: pointer to malloc'd space with copied string
+  */
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
