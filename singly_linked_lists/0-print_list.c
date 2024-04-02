2175698db9936283a9f60a1e700e02f3c7976269
yousrakdc <yousrakdc@gmail.com>
2024-04-02 11:49:56 +0000

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,18 +13,12 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-		return (n);
-	}
-
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
 			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
