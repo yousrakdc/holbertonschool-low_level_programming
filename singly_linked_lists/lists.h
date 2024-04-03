2c00257cdcd172a303e2c52f8087fc3f2db56372
yousrakdc <yousrakdc@gmail.com>
2024-04-03 08:40:53 +0000

    lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "lists.h"
 #include <stddef.h>
 /**
  * struct list_s - singly linked list
@@ -17,9 +16,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
