6ee22509eb244569b322595306bdf215a5059870
yousrakdc <yousrakdc@gmail.com>
2024-04-04 08:27:20 +0000

    print binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,28 +4,15 @@
 
 /**
  * print_binary - prints the binary representation of a number
+ * @n: integer
  * Return: success
  */
 
 void print_binary(unsigned long int n)
+
 {
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	unsigned long int size = sizeof(unsigned long int) * 8;
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 0; i < (int)size; i++)
-	{
-
-		if ((n & mask) == 0)
-		{
-			putchar('0');
-		}
-		else
-		{
-			putchar('1');
-		}
-
-		mask >>= 1;
-	}
-	putchar('\n');
+	putchar((n & 1) + '0');
 }
